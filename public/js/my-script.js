77663e832cacaff8c3aff46bc2e61593a33d304f
Oscar Werle <oscar.werle@gmail.com>
2023-07-13 08:03:14 +0200

    test features

--- public/js/my-script.js
+++ public/js/my-script.js
@@ -1,7 +1,19 @@
+// Log a message indicating the start of HTML element creation
 console.log("début de la création des éléments HTML");
+
+// Select the HTML element with the id "userSelect" and assign it to the variable `userSelect`
 const userSelect = document.querySelector('#userSelect');
-// Store tasks
+
+// Declare an empty array called `tasks` to store tasks
 let tasks = [];
+// Function to delete a task
+function deleteTask(task) {
+  const taskIndex = tasks.indexOf(task);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    renderTasks();
+  }
+}
 
 // Function to render tasks
 function renderTasks() {
@@ -23,30 +35,20 @@ function renderTasks() {
     } else {
       tasksTodoList.appendChild(taskItem);
     }
+
+    // Create a delete button for each task
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteTask(task);
+    });
+    taskItem.appendChild(deleteButton);
   });
 }
 
-// Function to handle task creation
-function createTask(e) {
-  e.preventDefault();
+// Rest of your code...
 
-  const taskText = taskInput.value;
-  const taskCategory = categorySelect.value;
-
-  if (taskText.trim() !== '') {
-    const newTask = {
-      text: taskText,
-      category: taskCategory,
-      completed: false
-    };
-
-    tasks.push(newTask);
-    renderTasks();
-    taskInput.value = '';
-  }
-}
-
-// Event listeners
+// Event listener for `change` event on `userSelect` element
 userSelect.addEventListener('change', () => {
   // Update tasks based on selected user
   // Replace with your logic to fetch user-specific tasks
@@ -54,127 +56,11 @@ userSelect.addEventListener('change', () => {
   renderTasks();
 });
 
+// Event listener for `submit` event on `taskForm` element
 taskForm.addEventListener('submit', createTask);
 
+// Event listener for `change` event on `categoryFilter` element
 categoryFilter.addEventListener('change', renderTasks);
 
-console.log("my-script.js");
-
-// Path to your JSON file on the server
-const filePath = '../../database/data.json';
-
-function readTodos() {
-    return fetch(filePath)
-      .then(response => response.json())
-      .catch(error => {
-        console.error('Error reading todos from file:', error);
-        return { todos: [] };
-      });
-  }
-  
-  function writeTodos(todos) {
-    return fetch('/database/save-todos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(todos)
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Error writing todos to file');
-        }
-        console.log('Todos saved to file.');
-      })
-      .catch(error => {
-        console.error('Error writing todos to file:', error);
-      });
-  }
-  
-  // Read todos from file
-  readTodos()
-    .then(data => {
-      const todos = data.todos;
-  
-      userSelect.addEventListener('change', function() {
-        const selectedUserId = userSelect.value;
-        const selectedUserTodos = todos.filter(todo => todo.userID === selectedUserId);
-  
-        // Use the selectedUserTodos array for further processing or rendering on your page
-        console.log(selectedUserTodos);
-      });
-  
-      const newTodo = {
-        id: '7ab3b1d3-a4e0-48db-9690-319d2735cd50',
-        txt: 'test',
-        cat: 'urgent',
-        done: false,
-        userID: '9df455e5-a4db-4cc5-896e-8134016de60e'
-      };
-  
-      console.log("new todo ", newTodo);
-      todos.push(newTodo);
-  
-      // Write todos to file
-      writeTodos({ todos });
-  
-      // Store tasks
-      let tasks = [];
-  
-      // Function to render tasks
-      function renderTasks() {
-        // Clear task lists
-        tasksTodoList.innerHTML = '';
-        tasksDoneList.innerHTML = '';
-  
-        // Filter tasks based on selected category
-        const selectedCategory = categoryFilter.value;
-        const filteredTasks = selectedCategory === 'all' ? tasks : tasks.filter(task => task.category === selectedCategory);
-  
-        // Render tasks in respective lists
-        filteredTasks.forEach(task => {
-          const taskItem = document.createElement('li');
-          taskItem.textContent = task.text;
-  
-          if (task.completed) {
-            tasksDoneList.appendChild(taskItem);
-          } else {
-            tasksTodoList.appendChild(taskItem);
-          }
-        });
-      }
-  
-      // Function to handle task creation
-      function createTask(e) {
-        e.preventDefault();
-  
-        const taskText = taskInput.value;
-        const taskCategory = categorySelect.value;
-  
-        if (taskText.trim() !== '') {
-          const newTask = {
-            text: taskText,
-            category: taskCategory,
-            completed: false
-          };
-  
-          tasks.push(newTask);
-          renderTasks();
-          taskInput.value = '';
-        }
-      }
-    //   const userSelect = document.querySelector('#userSelect');
-      // Event listeners
-      userSelect.addEventListener('change', () => {
-        // Update tasks based on selected user
-        // Replace with your logic to fetch user-specific tasks
-        tasks = [];
-        renderTasks();
-      });
-  
-      taskForm.addEventListener('submit', createTask);
-  
-      categoryFilter.addEventListener('change', renderTasks);
-  
-      console.log("my-script.js");
-    });
+// Log a message indicating that the `my-script.js` file has been executed
+console.log("my-script");
--- routing/index.js
+++ routing/index.js
@@ -8,3 +8,18 @@ router.use( '/api', apiRoutes );
 router.get('*', (req, res) => res.redirect('/home'));
 
 module.exports = router;
+
+// NODE - Todolist Per User
+// Échéance demain à 20:00
+// NODE
+
+// Instructions
+// Vous devez créer une application permettant de gérer plusieurs utilisateurs.
+// Chacun aura sa propre liste de tâche.
+
+// CONTRAINTES:
+//  • Au niveau de l'interface graphique, il faudra pouvoir sélectionner un utilisateur: ses tâches seront alors accessibles
+//  • Une tâche pourra être créée/catégorisée/faite/supprimée
+//  • Les tâches devront être affichée en 2 blocs: les taches à faire et faites
+//  • Un filtre permettra d'afficher uniquement les tâches d'une même catégorie
+//  • Les tâches devront être persistantes (fichier JSON ou mieux: BDD) (selon ce que vous vous sentez capables de faire)
